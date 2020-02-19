6fc05514385834739837aed86e14bb1d2a702e5a
Vincent DUGUET <vduguet@avidsen.com>
2020-02-19 16:48:29 +0100

    count

--- EventListeners/AddPagePlusListener.php
+++ EventListeners/AddPagePlusListener.php
@@ -9,6 +9,7 @@ use PagePlus\Event\PagePlusEvents;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\Event\Product\ProductEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Core\Event\ProductSaleElement\ProductSaleElementEvent;
 
@@ -23,10 +24,31 @@ class AddPagePlusListener implements EventSubscriberInterface
 
         );
     }
-
+    protected $request;
+    public function __construct(Request $request)
+    {
+      $this->request = $request;
+    }
+    
     public function notif(ProductEvent $event)
     {
-        $pagePlus = new PagePlus();
-        echo $pagePlus->getTitle;exit;
+        $request = $this->request;
+        print_r($_POST);exit;
+        $pagePlusProductId = $request->get('product_id');
+        $pagePlus = PagePlusQuery::create()->findByProductId($pagePlusProductId);
+        // si ca existe mise a jours
+        
+        for ($i=0; $i <= $count - 1; $i++ ) 
+        {
+            if(count($pagePlus) > 0) {
+            
+            }
+            else {
+                $newPagePlus = new PagePlus();
+    
+            }
+        }   
+
+
     }
 }
